ebc89b1abf6c3378eda7daf1e68675878ee73210
pmusaraj <pmusaraj@gmail.com>
2017-02-26 00:40:20 -0500

    remove urlScheme from global vars

--- global.js
+++ global.js
@@ -2,7 +2,5 @@
 global.siteDomain = 'yourdomain.com' 
 // the app's name, including as displayed in Home Screen
 global.appName = 'AppName'
-// url scheme, used to send links to your app
-global.urlScheme = 'appname'
 // App ID from OneSignal
 global.oneSignalAppId = 'ONESIGNAL-APP-ID-HERE'
